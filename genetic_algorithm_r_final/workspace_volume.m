648b29bbb39604c7737fda3f5ce068ce006ec23e
jbenand <88429989+jbenand@users.noreply.github.com>
2024-06-07 13:39:53 +0200

    Delete genetic_algorithm_r_final directory

--- genetic_algorithm_r_final/workspace_volume.m
+++
@@ -1,8 +0,0 @@
-%------------- Approximate workspace volume to pyramid with hexagonal base --------------
-
-% input: the positions in the workspace
-% ouput: the volume of the workspacem the ranges in x, y and z
-
-function [volume] = workspace_volume(dataMatrix)
-    [~, volume] = convhull(dataMatrix);
-end
